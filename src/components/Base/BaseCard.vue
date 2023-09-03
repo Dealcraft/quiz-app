ea7378c7006404ffbf53879b63983ce59790337f
David <david.loh18.dl@gmail.com>
2023-09-03 13:35:10 +0200

    Base card template

---
+++ src/components/Base/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="card" :class="hover ? 'card:hover-able' : ''">
+		<h3>Hallo Welt</h3>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "BaseCard",
+	props: {
+		hover: {
+			type: Boolean,
+		},
+	},
+});
+</script>
+
+<style scoped lang="scss"></style>
